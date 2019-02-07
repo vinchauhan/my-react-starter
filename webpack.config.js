52df22b6150f986a8cc325c66dc8eb0a4a64753c
Vineet Chauhan <chauhan.vineet@gmail.com>
2019-02-06 22:27:50 -0700

    More changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,54 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const path = require("path");
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./src/index.html",
-    filename: "./index.html"
-    })
-    
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+const copyPlugin = new CopyWebpackPlugin([
+  {
+    from: "src/manifest.json",
+    transform: function(content, path) {
+      // generates the manifest file using the package.json informations
+      return Buffer.from(
+        JSON.stringify({
+          description: process.env.npm_package_description,
+          version: process.env.npm_package_version,
+          ...JSON.parse(content.toString())
+        })
+      );
+    }
+  }
+]);
+
 module.exports = {
-    entry: './src/index.js',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
+  entry: "./src/index.js",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-              
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-              }
-        ]
-    },
-    plugins: [htmlPlugin]
-}
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [new CleanWebpackPlugin(["build"]), htmlPlugin, copyPlugin]
+};
